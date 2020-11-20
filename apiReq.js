ce4fa8b37f99344957292e6c72148c15c25d1292
MMabin <therealmasonmabin@gmail.com>
2020-11-19 23:41:51 -0600

    Update apiReq.js

--- apiReq.js
+++ apiReq.js
@@ -8,6 +8,6 @@ function reqSingleTicker(symbol) {
     request('https://financialmodelingprep.com/api/v3/historical-price-full/'+ symbol + 
     '?from=2015-03-12&to=2019-03-12&apikey=81a3f7667917cdacff683f03c69b84ef', function(err, res, body) {
     response.send(body);
-    console.log(req.query['one']);
+    console.log(req.query['one'])})}
 
-}
+
