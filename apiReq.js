b4d89601d3b0975d26b9c832f382a3b20e3ecc1f
MMabin <therealmasonmabin@gmail.com>
2020-11-25 20:55:45 -0600

    Rearranged data from server
    made into a list of objects. one object for each ticker, also added percentage attribute to each ticker object.

--- apiReq.js
+++ apiReq.js
@@ -5,16 +5,22 @@ var async = require('async');
 exports.reqTickers = function(req, res) {
     var tickers = [];
     for (ticker in req.body) {
+        if (ticker != 'date') {
         tickers.push(ticker);
+        }
     }
+    console.log(req.body);
+    
     var tickerData = [];
+    
     async.map(tickers,
         function(ticker, callback) {
             request('https://financialmodelingprep.com/api/v3/historical-price-full/'+ ticker + 
             '?from=2020-11-19&to=2020-11-20&apikey=81a3f7667917cdacff683f03c69b84ef',
                 function(err, response, body) {
-                    console.log(body)
-                    tickerData.push(body);
+                    let data = JSON.parse(body);
+                    data['percentage'] = req.body[ticker];
+                    tickerData.push(data);
                     callback();
                 }
             )
@@ -24,7 +30,7 @@ exports.reqTickers = function(req, res) {
                 console.log('Error');
                 res.status
             }
-            res.send(JSON.stringify(tickerData));
+            res.send(tickerData);
     })
 }
 
