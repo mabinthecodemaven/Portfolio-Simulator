03a7aa2dd9dd3d2cd79b990f7d9ea37231440e03
MMabin <therealmasonmabin@gmail.com>
2020-11-29 00:00:50 -0600

    Icon&graph
    Added tab icon, graph concpet seems straightforward from thus.

--- client/script.js
+++ client/script.js
@@ -1,4 +1,7 @@
 bindGenerateButton();
+google.charts.load('current', {packages: ['corechart', 'line']});
+//google.charts.setOnLoadCallback(drawBasic);
+
 
 function bindGenerateButton() {
     document.getElementById('Generate').addEventListener('click', function(event) {    
@@ -21,6 +24,7 @@ function bindGenerateButton() {
             if (request.status >= 200 && request.status < 400) {
                 var response = JSON.parse(request.responseText);
                 console.log(response)
+                drawBasic();
             } else {
                 console.log('Error in network request: ' + req.statusText);
             }
@@ -29,3 +33,28 @@ function bindGenerateButton() {
         request.send(JSON.stringify(data));
     })
 }
+
+
+function drawBasic() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('date', 'X');
+      data.addColumn('number', 'Dogs');
+
+      data.addRows([
+        [new Date(2018, 9, 27), 69]
+      ]);
+
+      var options = {
+        hAxis: {
+          title: 'Time'
+        },
+        vAxis: {
+          title: 'Popularity'
+        }
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+      chart.draw(data, options);
+    }
