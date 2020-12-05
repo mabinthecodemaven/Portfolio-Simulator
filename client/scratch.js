5cb365d1e03e3b19bfb1841981666f5d3dd47dea
MMabin <therealmasonmabin@gmail.com>
2020-12-05 16:58:20 -0600

    Recursion Troubles
    sad face

--- client/scratch.js
+++ client/scratch.js
@@ -59,33 +59,53 @@ function drawBasic(table) {
     }
 
 
-function performance(stockData) {
-  let dataTable = {};
-  let tableOut = [];
+function performance(stockData, period=63, i=0, dataTable={}, balance=0) {
+    //Break case
+    if (i+period > stockData[0].historical.length) {
+        for (stock of stockData) {
+            let historical = stock.historical.reverse();
+            let shares = stock.percentage * balance * 0.01 / historical[i].open;
+            
+            for (;i<stock.historical.length;i++) {
+                let day = historical[i]
+                let amount = shares*day.close;
 
-  
-  for (stock of stockData) {
-    let historical = stock.historical.reverse()
-    let shares = stock.percentage / historical[0].open; 
-    //console.log(shares);
-    
-    for (day of historical) {
+                if (!(day.date in dataTable)) {
+                    dataTable[day.date] = amount;
+                }
+                else {
+                    dataTable[day.date] += amount;
+                }
+            }
+        }
+        let tableOut = [];
+        for (date in dataTable) {
+            let dateArray = date.split('-');
+            let datum = [new Date(dateArray[0], dateArray[1]-1, dateArray[2]), dataTable[date]];
+            tableOut.push(datum);
+        }
+        return tableOut;  
+  }
 
-      let amount = shares * day.close;
-      //console.log(amount);
-      if ( !(day.date in dataTable)) {
-        dataTable[day.date] = amount;
-      }
-      else {
-        dataTable[day.date] += amount;
-      }
+    else {
+        for (stock of stockData) {
+            let historical = stock.historical.reverse();
+            let shares = stock.percentage * balance * 0.01 / historical[i].open;
+            
+            for (;i<period;i++) {
+                let day = historical[i]
+                let amount = shares*day.close;
+
+                if (!(day.date in dataTable)) {
+                    dataTable[day.date] = amount;
+                }
+                else {
+                    dataTable[day.date] += amount;
+                }
+            }
+        }
+        balance = dataTable[stockData[0].historical.reverse()[i].date];
+        performance(stockData, period, i, balance);
+
     }
-  }
-  for (date in dataTable) {
-    let dateArray = date.split('-');
-    let datum = [new Date(dateArray[0], dateArray[1]-1, dateArray[2]), dataTable[date]];
-    tableOut.push(datum);
-  }
-  return tableOut;
-
 }
--- scratch.js
+++ scratch.js
@@ -1,18 +1,111 @@
-lst = [2,3,5,1264,'a'];
+bindGenerateButton();
+google.charts.load('current', {packages: ['corechart', 'line']});
+//google.charts.setOnLoadCallback(drawBasic);
 
-for (i=0; i<lst.length; i++) {
-  console.log(lst[i]);
+
+function bindGenerateButton() {
+    document.getElementById('Generate').addEventListener('click', function(event) {    
+        event.preventDefault();
+        let entries = Array.from(document.getElementsByClassName('ticker'));
+        var data = {};
+        for (entry of entries) {
+            let ticker = entry.firstElementChild;
+            if (ticker.value != '') {
+              let percentage = ticker.nextElementSibling;
+              data[ticker.value] = parseInt(percentage.value);
+            }
+        }
+        
+        var request = new XMLHttpRequest();
+        request.open('POST', '/getdata', true);
+        request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+        request.addEventListener('load', function() {
+            if (request.status >= 200 && request.status < 400) {
+                var response = JSON.parse(request.responseText);
+                //console.log(response);
+                //performance(response);
+
+                drawBasic(performance(response));
+            } else {
+                console.log('Error in network request: ' + req.statusText);
+            }
+        })
+        
+        request.send(JSON.stringify(data));
+    })
 }
 
-let date = "2020-11-20";
-//date = new Date(date);
-console.log(date);
 
-var ob = {'apple': 50};
-ob.apple += 25
-console.log(ob.apple);
+function drawBasic(table) {
 
-//let dateArray = day.date.split('-');
-      
-      
-  //    dataTable.push([new Date(dateArray[0], dateArray[1], dateArray[2]), day.close]);
+      var data = new google.visualization.DataTable();
+      data.addColumn('date', 'X');
+      data.addColumn('number', 'Balance');
+
+      data.addRows(table);
+
+      var options = {
+        hAxis: {
+          title: 'Time'
+        },
+        vAxis: {
+          title: 'Balance'
+        }
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+      chart.draw(data, options);
+    }
+
+
+function performance(stockData, period=63, i=0, dataTable={}, balance=0) {
+    //Break case
+    if (i+period > stockData[0].historical.length) {
+        for (stock of stockData) {
+            let historical = stock.historical.reverse();
+            let shares = stock.percentage * balance * 0.01 / historical[i].open;
+            
+            for (;i<stock.historical.length;i++) {
+                let day = historical[i]
+                let amount = shares*day.close;
+
+                if (!(day.date in dataTable)) {
+                    dataTable[day.date] = amount;
+                }
+                else {
+                    dataTable[day.date] += amount;
+                }
+            }
+        }
+        let tableOut = [];
+        for (date in dataTable) {
+            let dateArray = date.split('-');
+            let datum = [new Date(dateArray[0], dateArray[1]-1, dateArray[2]), dataTable[date]];
+            tableOut.push(datum);
+        }
+        return tableOut;  
+  }
+
+    else {
+        for (stock of stockData) {
+            let historical = stock.historical.reverse();
+            let shares = stock.percentage * balance * 0.01 / historical[i].open;
+            
+            for (;i<period;i++) {
+                let day = historical[i]
+                let amount = shares*day.close;
+
+                if (!(day.date in dataTable)) {
+                    dataTable[day.date] = amount;
+                }
+                else {
+                    dataTable[day.date] += amount;
+                }
+            }
+        }
+        balance = dataTable[stockData[0].historical.reverse()[i].date];
+        console.log(i);
+
+    }
+}
